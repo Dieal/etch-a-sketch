6dff528beb94885b1c3de1b3dc2349865bf3fe49
Dieal <59124097+Dieal@users.noreply.github.com>
2023-07-29 00:30:27 +0200

    Add coloring feature to the grid
    This will allow the user to
    draw on the grid by
    moving the mouse over it

--- sketch.js
+++ sketch.js
@@ -1,5 +1,12 @@
 createGrid();
 
+const squares = document.querySelectorAll("div.grid-container > div");
+squares.forEach(square => square.addEventListener("mouseover", colorSquare));
+
+function colorSquare(e) {
+    this.style.backgroundColor = "black";
+}
+
 function createGrid() {
     const container = document.querySelector("div.grid-container");
     for (let i = 0; i < (16 * 16); i++) {
